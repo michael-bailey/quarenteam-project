96dd01fc9548e0abaea4d76a0e4083cfa315430b
michael-bailey <mickyb18a@gmail.com>
2021-09-30 10:30:05 +0100

    added main application with bootstrap css

---
+++ src/client/pages/_app.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// This is the main application fot the next app
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			{/* head of the html bage */}
+			<Head>
+				<title>Quarenteam</title>
+			</Head>
+			{/* global page structure can go here */}
+
+			{/* the page being loaded */}
+			<Component {...pageProps} />
+		</>
+	);
+}
+
+export default MyApp;
