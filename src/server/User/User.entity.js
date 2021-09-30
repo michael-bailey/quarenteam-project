867642382c7837f6fc78701b7354922f979858f1
michael-bailey <mickyb18a@gmail.com>
2021-09-30 11:10:05 +0100

    added base sequelize and user class

---
+++ src/server/User/User.entity.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../sequelize.js');
+
+class User extends Model {}
+User.init(
+	{
+		username: DataTypes.STRING,
+		email: DataTypes.STRING,
+	},
+	{ sequelize, modelName: 'user' }
+);
+
+module.exports = {
+	User,
+};
