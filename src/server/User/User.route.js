782e37574198348d9e51cd13ed6d488daf91f17c
michael-bailey <mickyb18a@gmail.com>
2021-09-30 12:23:49 +0100

    added basic crud api endpoints

--- src/server/User/User.route.js
+++ src/server/User/User.route.js
@@ -1,6 +1,18 @@
 const { Router } = require('express');
 const { User } = require('./User.entity');
 
+/**
+ * # UserRouter '/backend/user'
+ *
+ * Controls all user api
+ * - route '/':
+ * 	- get: returns all users
+ * 	- post: creates a new user
+ * - route '/:id'
+ *  - get: get the user by id
+ * 	- patch: update user by id
+ *  - delete: delete a user by id
+ */
 const UserRouter = Router('/user');
 
 UserRouter.route('/')
@@ -9,16 +21,21 @@ UserRouter.route('/')
 		res.json(users);
 	})
 	.post(async (req, res) => {
-		let user = await User.create({
-			username: 'michael',
-			email: 'email@noreply.com',
-		});
+		let user = await User.create(req.body);
 		res.json(user);
 	});
 
-UserRouter.route('/:id').get((req, res) => {
-	res.send('this is the id');
-});
+UserRouter.route('/:id')
+	.get(async (req, res) => {
+		let user = await User.findByPk(req.params.id);
+		res.json(user);
+	})
+	.patch(async (req, res) => {
+		User.update(req.body, { where: { id: req.params.id } });
+	})
+	.delete(async (req, res) => {
+		User.destroy({ where: { id: req.params.id } });
+	});
 
 module.exports = {
 	UserRouter,
