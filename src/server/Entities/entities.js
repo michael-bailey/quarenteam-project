dcf542ad1ef021374fdc8b4d46a69741783a28b5
michael-bailey <mickyb18a@gmail.com>
2021-10-01 09:57:06 +0100

    added quiz attempt and user relations

--- src/server/Entities/entities.js
+++ src/server/Entities/entities.js
@@ -1,6 +1,9 @@
 const { User } = require('../User/User.entity');
 const { QuizAttempt } = require('../QuizAttempt/QuizAttempt.entity');
 
+User.hasMany(QuizAttempt);
+QuizAttempt.belongsTo(User);
+
 module.exports = {
 	User,
 	QuizAttempt,
