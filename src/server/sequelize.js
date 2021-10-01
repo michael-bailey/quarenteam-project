65c853d13a6e84f16d43a01e658a5831e4326732
michael-bailey <mickyb18a@gmail.com>
2021-10-01 12:16:23 +0100

    added mysql to databases not for testing

--- src/server/sequelize.js
+++ src/server/sequelize.js
@@ -1,4 +1,8 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
 
-module.exports = sequelize;
+let sequelize;
+
+module.exports =
+	process.env.NODE_ENV == 'test'
+		? new Sequelize('sqlite::memory:')
+		: new Sequelize('mysql://root:toor@localhost:3306/quarenteam');
